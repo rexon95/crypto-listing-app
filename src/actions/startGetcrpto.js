45a6b9a4e4f0b351e4777bbbaef264d1c2f66ff4
Rexon Dmello <57930491+rexon95@users.noreply.github.com>
2021-05-26 17:11:29 +0530

    Update startGetcrpto.js

--- src/actions/startGetcrpto.js
+++ src/actions/startGetcrpto.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 export const getCrpto = () =>{
 
     return (dispatch) => {
-          axios.get('https://dct-cors.herokuapp.com/api.nomics.com/v1/currencies/ticker?key=e452014e4ea719517836beba67b416361f1806c5&interval=1d&convert=INR')
+          axios.get('https://dct-cors.herokuapp.com/https://api.nomics.com/v1/currencies/ticker?key=e452014e4ea719517836beba67b416361f1806c5&interval=1d&convert=INR')
           .then(res=>{
               console.log(res.data)
               dispatch(setData(res.data))
